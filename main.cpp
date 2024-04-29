a63032a51ff27de3fecaaf1a1ac983174a21ac7d
Preetom <167507439+PreetomBoro@users.noreply.github.com>
2024-04-30 01:18:34 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,20 +15,22 @@ GLFWwindow* initglfw(const char* title)
     return dummy;
 }
 
+
+//bad idea. very bad idea, several errors. don't forget to put it inside int main.
 void initimgui(GLFWwindow* window)
 {
     //DearImGUI init
     IMGUI_CHECKVERSION(); //<---not really sure how this affects the project (leave it here)
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
-    ImGui::StyleColorsLight();                                // Style (classic is also good)
+    ImGuiIO& io = ImGui::GetIO(); (void)io;			// this identifier seems important inside the loop, can't have it here alone. bad idea
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     	// Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      	// Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         	// Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       	// Enable Multi-Viewport / Platform Windows
+    ImGui::StyleColorsLight();                                	// Style (classic is also good)
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+    ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());	//gives error when not found in between ImGui begin and end
     return;
 }
 
