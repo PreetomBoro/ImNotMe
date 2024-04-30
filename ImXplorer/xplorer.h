08291be780351fc01f0f990b396f90574fdbcec4
Preetom Boro <gitpreetomboro@gmail.com>
2024-05-01 02:09:19 +0530

    need to work on file system stuff

--- ImXplorer/xplorer.h
+++ ImXplorer/xplorer.h
@@ -1,6 +1,6 @@
 static void HelpMarker(const char* desc);
 namespace XPLORER
 {
-    void ShowXplorer();
-
+    void ShowXplorer(); //shows xplorer window
+    struct Shortcut_data; //will hold the paths for the shortcut; has add_path(), remove_path(), update();
 }
