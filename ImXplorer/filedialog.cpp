e3ab7787427160260bd8462307915dfcdd29743a
Preetom Boro <gitpreetomboro@gmail.com>
2024-05-02 02:49:13 +0530

    done

--- ImXplorer/filedialog.cpp
+++ ImXplorer/filedialog.cpp
@@ -1,6 +1,6 @@
 #include "filedialog.h"
 #include "imgui.h"
-#include "imfilebrowser.h"
+
 #include <iostream>
 
 namespace FDIALOG
@@ -8,24 +8,3 @@ namespace FDIALOG
 
 }
 
-
-//using the project "imgui-filebrowser" for now
-
-void imfilebrowser()
-{
-    // create a file browser instance
-    ImGui::FileBrowser fileDialog;
-
-    // (optional) set browser properties
-    fileDialog.SetTitle("title");
-    fileDialog.SetTypeFilters({ ".h", ".cpp" });
-
-    fileDialog.Open();
-    fileDialog.Display();
-
-    if(fileDialog.HasSelected())
-    {
-        std::cout << "Selected filename" << fileDialog.GetSelected().string() << std::endl;
-        fileDialog.ClearSelected();
-    }
-}
